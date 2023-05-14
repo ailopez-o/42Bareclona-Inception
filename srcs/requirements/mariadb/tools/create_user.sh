079d474e5fa1a0b39c4f9cfa0bcbe66a641a69fb
ailopez-o <aitorlpzaudikana@gmail.com>
2023-05-15 01:04:52 +0200

    mysql

--- srcs/requirements/mariadb/tools/create_user.sh
+++ srcs/requirements/mariadb/tools/create_user.sh
@@ -2,14 +2,15 @@
 
 service mysql start
 
-mysql -u mysql -e "CREATE DATABASE ${DB_NAME};"
-echo "CREATE DATABASE ${DB_NAME};"
-mysql -u mysql -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-echo "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-mysql -u mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
-echo "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
-mysql -u mysql -e "FLUSH PRIVILEGES;"
-mysql -u mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
-echo "ALTER USER 'mysql'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+# Espera a que el servidor esté listo
+while ! mysqladmin ping -hlocalhost --silent; do
+    sleep 1
+done
+
+mysql -e "CREATE DATABASE ${DB_NAME};"
+mysql -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
+mysql -e "FLUSH PRIVILEGES;"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
 
 
