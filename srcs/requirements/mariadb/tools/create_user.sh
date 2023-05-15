70f591ca7ca037c53fd2f651262c27992c915308
ailopez-o <aitorlpzaudikana@gmail.com>
2023-05-15 10:47:39 +0200

    mysql

--- srcs/requirements/mariadb/tools/create_user.sh
+++ srcs/requirements/mariadb/tools/create_user.sh
@@ -3,15 +3,11 @@
 # chown -R mysql:mysql /var/lib/mysql
 
 service mysql start
-mysql -u root -e "DROP DATABASE IF EXISTS $MYSQL_DATABASE;"
-echo $MYSQL_DATABASE $MYSQL_USER $MYSQL_PASSWORD $MYSQL_ROOT_PASSWORD > log.txt
-mysql -u root -e "DROP USER IF EXISTS '$MYSQL_USER'@'%';"
 mysql -u root -e "CREATE DATABASE $MYSQL_DATABASE;"
 mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
 mysql -u root -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
 mysql -u root -e "FLUSH PRIVILEGES;"
 mysqladmin -u root password "$MYSQL_ROOT_PASSWORD"
-# service mysql stop
 
 
 
