a2a90a971c4e86cebb56200efc443eda3c8be96d
ailopez-o <aitorlpzaudikana@gmail.com>
2023-05-15 09:52:46 +0200

    mysql

--- srcs/requirements/mariadb/tools/create_user.sh
+++ srcs/requirements/mariadb/tools/create_user.sh
@@ -18,15 +18,14 @@ useradd -u 999 mysql
 chown -R mysql:mysql /var/lib/mysql
 
 service mysql start
-#mysql -u root -e "DROP DATABASE IF EXISTS incepionbbdd;"
-# mysql -u root -e "DROP USER IF EXISTS 'nolan'@'%';"
-echo mi variable $DB_NAME >> /log.txt
-#mysql -u root -e "CREATE DATABASE $DB_NAME;"
-# mysql -u root -e "CREATE USER 'nolan'@'%' IDENTIFIED BY '123456'"
-# mysql -u root -e "GRANT ALL ON incepionbbdd.* TO 'nolan'@'%' IDENTIFIED BY '123456' WITH GRANT OPTION;"
-# mysql -u root -e "FLUSH PRIVILEGES;"
-# mysqladmin -u root password "123456"
-#service mysql stop
+mysql -u root -e "DROP DATABASE IF EXISTS $MYSQL_DATABASE;"
+mysql -u root -e "DROP USER IF EXISTS '$MYSQL_USER'@'%';"
+mysql -u root -e "CREATE DATABASE $MYSQL_DATABASE;"
+mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
+mysql -u root -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
+mysql -u root -e "FLUSH PRIVILEGES;"
+mysqladmin -u root password "$MYSQL_ROOT_PASSWORD"
+# service mysql stop
 
 
 
