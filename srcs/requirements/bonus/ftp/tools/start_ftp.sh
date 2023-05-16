e54e91b5f2620e9f4bfae49dc71d9061032b2c87
Aitor Lopez de audicana oar <aitorlpzaudikana@gmail.com>
2023-05-16 20:34:41 +0200

    ftp starting

--- srcs/requirements/bonus/ftp/tools/start_ftp.sh
+++ srcs/requirements/bonus/ftp/tools/start_ftp.sh
@@ -7,15 +7,16 @@ if [ ! -f "/etc/vsftpd/vsftpd.conf.bak" ]; then
     cp /etc/vsftpd/vsftpd.conf /etc/vsftpd/vsftpd.conf.bak
     mv /tmp/vsftpd.conf /etc/vsftpd/vsftpd.conf
 
-    # Add the FTP_USER, change his password and declare him as the owner of wordpress folder and all subfolders
-    adduser $FTP_USR --disabled-password
-    echo "$FTP_USR:$FTP_PASSWORD" | /usr/sbin/chpasswd
-    chown -R $FTP_USR:$FTP_USR /var/www/html
+    # # Add the FTP_USER, change his password and declare him as the owner of wordpress folder and all subfolders
+    # adduser $FTP_USR --disabled-password
+    # echo "$FTP_USR:$FTP_PASSWORD" | /usr/sbin/chpasswd
+    # chown -R $FTP_USR:$FTP_USR /var/www/html
 
-	#chmod +x /etc/vsftpd/vsftpd.conf
-    echo $FTP_USR | tee -a /etc/vsftpd.userlist
+	# #chmod +x /etc/vsftpd/vsftpd.conf
+    # echo $FTP_USR | tee -a /etc/vsftpd.userlist
 else
 	echo "FTP already configured"
 fi
 echo "FTP started on :21"
-/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
+vsftpd /etc/vsftpd/vsftpd.conf
+tail -f /dev/null
