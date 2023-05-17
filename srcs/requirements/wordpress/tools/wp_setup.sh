c75abf72aa0d9aff1693f9d323c1a1b91894864b
ailopez-o <aitorlpzaudikana@gmail.com>
2023-05-17 18:06:47 +0200

    wp user

--- srcs/requirements/wordpress/tools/wp_setup.sh
+++ srcs/requirements/wordpress/tools/wp_setup.sh
@@ -2,6 +2,8 @@
 if [ ! -f "/var/www/html/config.php" ]; then
 	echo "Seting up wordpress"
 	cd /var/www/html
+	echo "Setting Up Admin User $WP_ADMIN_USR - $WP_ADMIN_PWD"
+	echo "Setting Up User $WP_USR - $WP_PWD"
 	wp core download --allow-root
 	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
 	wp core install --url=$DOMAIN_NAME --title="$WP_TITLE" --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
