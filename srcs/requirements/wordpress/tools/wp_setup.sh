716b3285fde818c5dd07def23520e54a10403dba
ailopez-o <aitorlpzaudikana@gmail.com>
2023-05-17 18:13:04 +0200

    wp user

--- srcs/requirements/wordpress/tools/wp_setup.sh
+++ srcs/requirements/wordpress/tools/wp_setup.sh
@@ -2,12 +2,12 @@
 if [ ! -f "/var/www/html/config.php" ]; then
 	echo "Seting up wordpress"
 	cd /var/www/html
-	echo "Setting Up Admin User $WP_ADMIN_USR - $WP_ADMIN_PWD"
-	echo "Setting Up User $WP_USR - $WP_PWD"
+	echo "Setting Up Admin User $WP_ADMIN_USER - $WP_ADMIN_PASSWORD"
+	echo "Setting Up User $WP_USER - $WP_USER_PASSWORD"
 	wp core download --allow-root
 	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-	wp core install --url=$DOMAIN_NAME --title="$WP_TITLE" --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-	wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+	wp core install --url=$DOMAIN_NAME --title="$WP_TITLE" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+	wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
 	wp theme install storia --activate --allow-root
 else
 	echo "Wordpress allready configured"
