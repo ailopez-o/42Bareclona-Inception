ffd6d38529ba912e15d3d9023bcfdbed3a2af68f
Aitor López de Audícana <102803124+ailopez-o@users.noreply.github.com>
2024-05-14 14:48:34 +0200

    Update start.sh cert

--- srcs/requirements/nginx/tools/start.sh
+++ srcs/requirements/nginx/tools/start.sh
@@ -3,7 +3,14 @@
 # Ejecutar Certbot
 #certbot --nginx -d netard.net -d www.netard.net --non-interactive --agree-tos --email aitorlpzaudikana@gmail.com
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=ES/L=Barcelona/O=1337/OU=student/CN=netard.net"
+RUN openssl req \
+            -x509 \
+            -nodes \
+            -days 365 \
+            -newkey rsa:2048 \
+            -keyout /etc/ssl/private/nginx-selfsigned.key \
+            -out /etc/ssl/certs/nginx-selfsigned.crt \
+            -subj '/C=ES/ST=Barcelona/L=Barcelona/OU=42Barcelona/CN=netard.net'
 
 # Iniciar Nginx
 nginx -g "daemon off;"
